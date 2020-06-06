dae2895a1362d41e5d375999604745a172c6bb72
Alexei Spirit <alexeispirit@gmail.com>
2020-06-06 21:18:12 +0500

    28-edit-mutation

--- 01-users/schema/schema.js
+++ 01-users/schema/schema.js
@@ -95,6 +95,20 @@ const mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLString },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
