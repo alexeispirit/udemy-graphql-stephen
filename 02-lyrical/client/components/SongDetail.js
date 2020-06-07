800825d0f024cbffe771d655c85ebc8e58a5d0a5
Alexei Spirit <alexeispirit@gmail.com>
2020-06-07 19:07:43 +0500

    64-showing-list-of-lyrics

--- 02-lyrical/client/components/SongDetail.js
+++ 02-lyrical/client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { graphql } from "react-apollo";
 import { Link } from "react-router";
 
 import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 import fetchSong from "../queries/fetchSong";
 
 class SongDetail extends Component {
@@ -17,6 +18,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
