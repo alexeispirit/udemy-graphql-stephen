bbe5fdbde484583df2f645ed42d66206e9e6b913
Alexei Spirit <alexeispirit@gmail.com>
2020-06-09 17:53:55 +0500

    95-automatic-component-rerenders

--- 03-auth/client/components/Header.js
+++ 03-auth/client/components/Header.js
@@ -7,7 +7,9 @@ import query from "../queries/currentUser";
 
 class Header extends Component {
   onLogoutClick() {
-    this.props.mutate({});
+    this.props.mutate({
+      refetchQueries: [{ query }],
+    });
   }
 
   renderButtons() {
